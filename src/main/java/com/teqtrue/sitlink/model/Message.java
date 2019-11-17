d863d7a21d579ab9b960d2607972cb3df22692bb
Fry98 <filip-toman@seznam.cz>
2019-11-17 21:55:11 +0100

    Fixed Cheateks fucked up JPA entitites

--- src/main/java/com/teqtrue/sitlink/model/Message.java
+++ src/main/java/com/teqtrue/sitlink/model/Message.java
@@ -1,16 +1,20 @@
 package com.teqtrue.sitlink.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
+@Entity
 public class Message {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
 
-  @Column(nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY)
   private User sender;
 
   @Column(nullable = false)
--- src/main/java/com/teqtrue/sitlink/model/Subchat.java
+++ src/main/java/com/teqtrue/sitlink/model/Subchat.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
+@Entity
 public class Subchat {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,7 +24,7 @@ public class Subchat {
   @Column(nullable = false)
   private String description;
 
-  @Column(nullable = false)
+  @ManyToOne(fetch = FetchType.LAZY)
   private User admin;
 
   @OneToMany(cascade = CascadeType.REMOVE)
