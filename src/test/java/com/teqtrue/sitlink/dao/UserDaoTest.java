16a4e4d81fcdeef9a3ff7db8b1ff058cbf10e334
Fry98 <filip-toman@seznam.cz>
2019-11-17 23:02:29 +0100

    Added UserDao tests

---
+++ src/test/java/com/teqtrue/sitlink/dao/UserDaoTest.java
@@ -0,0 +1,68 @@
+package com.teqtrue.sitlink.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertTrue;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import com.teqtrue.sitlink.App;
+import com.teqtrue.sitlink.model.User;
+
+// For explanatory comments, see ProductDaoTest
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ComponentScan(basePackageClasses = App.class)
+public class UserDaoTest {
+
+  @PersistenceContext
+  private EntityManager em;
+
+  @Autowired
+  private UserDao dao;
+
+  @Test
+  public void persistCreatesNewUserWithMatchingData() {
+    User newUser = new User(
+      "test",
+      "te@s.t",
+      "superduperurl",
+      "hashedgarbage"
+    );
+    dao.persist(newUser);
+    
+    User userFromDb = em.find(User.class, newUser.getId());
+    assertTrue(userFromDb != null);
+    assertTrue(userFromDb.getNick() == "test");
+    assertTrue(userFromDb.getMail() == "te@s.t");
+    assertTrue(userFromDb.getImg() == "superduperurl");
+    assertTrue(userFromDb.getPassword() == "hashedgarbage");
+  }
+
+  @Test
+  public void findByNickReturnsPersonWithMatchingNickname() {
+    em.persist(new User(
+      "test",
+      "te@s.t",
+      "superduperurl",
+      "hashedgarbage"
+    ));
+    User userFromDb = dao.findByNick("test");
+    assertTrue(userFromDb != null);
+    assertTrue(userFromDb.getNick() == "test");
+    assertTrue(userFromDb.getMail() == "te@s.t");
+    assertTrue(userFromDb.getImg() == "superduperurl");
+    assertTrue(userFromDb.getPassword() == "hashedgarbage");
+  }
+
+  @Test
+  public void findByNickReturnsNullForUnknownNickname() {
+    User userFromDb = dao.findByNick("nonexistent");
+    assertTrue(userFromDb == null);
+  }
+}
