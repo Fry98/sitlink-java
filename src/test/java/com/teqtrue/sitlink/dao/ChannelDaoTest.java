368af8da8d30fc02ecc4604a85cad83d3000d7bd
vitriha <vit.riha@aspectworks.com>
2019-11-24 14:38:36 +0100

    added Dao tests

---
+++ src/test/java/com/teqtrue/sitlink/dao/ChannelDaoTest.java
@@ -0,0 +1,80 @@
+package com.teqtrue.sitlink.dao;
+
+import com.teqtrue.sitlink.App;
+import com.teqtrue.sitlink.model.Channel;
+import com.teqtrue.sitlink.model.Subchat;
+import com.teqtrue.sitlink.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ComponentScan(basePackageClasses = App.class)
+public class ChannelDaoTest {
+
+  @PersistenceContext
+  private EntityManager em;
+
+  @Autowired
+  private ChannelDao dao;
+
+  private Channel prepareChannel() {
+    return new Channel(
+        "test channel"
+    );
+  }
+
+  private Subchat prepareSubchat() {
+    User admin = new User(
+        "admin",
+        "ad@m.in",
+        "veryniceurl",
+        "hashedpwd"
+    );
+    em.persist(admin);
+
+    return new Subchat(
+        "test subchat",
+        "test description",
+        admin
+    );
+  }
+
+  @Test
+  public void saveCreatesNewChannelWithMatchingData() {
+    Channel testChannel = prepareChannel();
+
+    dao.save(testChannel);
+
+    Channel channelFromDb = em.find(Channel.class, testChannel.getId());
+
+    assertNotEquals(null, channelFromDb);
+    assertEquals("test channel", channelFromDb.getName());
+  }
+
+  @Test
+  public void findByNameAndSubchatTitleReturnsChannelWithMatchingNameAndSubchat() {
+    Channel testChannel = prepareChannel();
+    Subchat testSubchat = prepareSubchat();
+
+    testSubchat.addChannel(testChannel);
+
+    em.persist(testChannel);
+    em.persist(testSubchat);
+
+    Channel channelFromDb = dao.findByNameAndSubchatTitle("test channel", "test subchat");
+
+    assertNotEquals(null, channelFromDb);
+    assertEquals("test channel", channelFromDb.getName());
+  }
+}
---
+++ src/test/java/com/teqtrue/sitlink/dao/SubchatDaoTest.java
@@ -0,0 +1,81 @@
+package com.teqtrue.sitlink.dao;
+
+import com.teqtrue.sitlink.App;
+import com.teqtrue.sitlink.model.Subchat;
+import com.teqtrue.sitlink.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@ComponentScan(basePackageClasses = App.class)
+public class SubchatDaoTest {
+
+  @PersistenceContext
+  private EntityManager em;
+
+  @Autowired
+  private SubchatDao dao;
+
+  @Autowired
+  private UserDao userDao;
+
+  private Subchat prepareSubchat(User admin) {
+    return new Subchat(
+        "test subchat",
+        "This is a test subchat",
+        admin
+    );
+  }
+
+  private User prepareUser() {
+    return new User(
+        "test",
+        "te@s.t",
+        "superduperurl",
+        "hashedgarbage"
+    );
+  }
+
+  @Test
+  public void saveCreatesNewSubchatWithMatchingData() {
+    User testUser = prepareUser();
+    Subchat testSubchat = prepareSubchat(testUser);
+
+    userDao.save(testUser);
+    dao.save(testSubchat);
+
+    Subchat subchatFromDb = em.find(Subchat.class, testSubchat.getId());
+
+    assertNotEquals(null, subchatFromDb);
+    assertEquals("test subchat", subchatFromDb.getTitle());
+    assertEquals("This is a test subchat", subchatFromDb.getDesc());
+    assertEquals(testUser.getId(), subchatFromDb.getAdmin().getId());
+  }
+
+  @Test
+  public void findByTitleReturnsSubchatWithMatchingTitle() {
+    User admin = prepareUser();
+    Subchat testSubchat = prepareSubchat(admin);
+
+    em.persist(admin);
+    em.persist(testSubchat);
+
+    Subchat subchatFromDb = dao.findByTitle("test subchat");
+
+    assertNotEquals(null, subchatFromDb);
+    assertEquals("test subchat", subchatFromDb.getTitle());
+    assertEquals("This is a test subchat", subchatFromDb.getDesc());
+    assertEquals(admin.getId(), subchatFromDb.getAdmin().getId());
+  }
+}
