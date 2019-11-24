6a4e097e7a73c39b9ea202a54cb5a526c212711b
vitriha <vit.riha@aspectworks.com>
2019-11-24 15:13:26 +0100

    added UserService test

---
+++ src/test/java/com/teqtrue/sitlink/services/UserServiceTest.java
@@ -0,0 +1,96 @@
+package com.teqtrue.sitlink.services;
+
+import com.teqtrue.sitlink.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class UserServiceTest {
+
+  @PersistenceContext
+  private EntityManager em;
+
+  @Autowired
+  private UserService userService;
+
+  @Test
+  public void createUserCreatesNewUserInDatabase() {
+    final User user = userService.createUser(
+        "test user",
+        "test@user.org",
+        "url.org",
+        "plainPassword"
+    );
+
+    final User userFromDb = em.find(User.class, user.getId());
+
+    assertNotEquals(null, userFromDb);
+    assertEquals("test user", userFromDb.getNick());
+    assertEquals("test@user.org", userFromDb.getMail());
+    assertEquals("url.org", userFromDb.getImg());
+  }
+
+  @Test
+  public void createdUsersPasswordIsNotStoredAsPlainText() {
+    final String plainPassword = "plainPassword";
+    final User user = userService.createUser(
+        "test user",
+        "test@user.org",
+        "url.org",
+        plainPassword
+    );
+
+    final User result = em.find(User.class, user.getId());
+
+    assertNotEquals(plainPassword, result.getPassword());
+  }
+
+  @Test
+  public void authUserReturnsTrueToCorrectPassword() {
+    final String plainPassword = "plainPassword";
+    final User user = userService.createUser(
+        "test user",
+        "test@user.org",
+        "url.org",
+        plainPassword
+    );
+
+    assertTrue(userService.authUser(user, plainPassword));
+  }
+
+  @Test
+  public void authUserReturnsFalseToIncorrectPassword() {
+    final String plainPassword = "plainPassword";
+    final User user = userService.createUser(
+        "test user",
+        "test@user.org",
+        "url.org",
+        plainPassword
+    );
+
+    assertFalse(userService.authUser(user, "incorrectPassword"));
+  }
+
+  @Test
+  public void createRootCreatesRootUserWithAllPropertiesSetToRoot() {
+    User root = userService.createRoot();
+
+    assertEquals("root", root.getNick());
+    assertEquals("root", root.getMail());
+    assertEquals("root", root.getImg());
+    assertEquals("root", root.getPassword());
+  }
+}
